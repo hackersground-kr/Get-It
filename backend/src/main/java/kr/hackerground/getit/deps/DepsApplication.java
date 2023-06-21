33ef6a70791befea8fa8633294e6d41f5df65fb2
pmh-only <opensource@pmh.codes>
2023-06-21 15:36:09 +0900

    feat: init backend

---
+++ backend/src/main/java/kr/hackerground/getit/deps/DepsApplication.java
@@ -0,0 +1,13 @@
+package kr.hackerground.getit.deps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepsApplication.class, args);
+	}
+
+}
