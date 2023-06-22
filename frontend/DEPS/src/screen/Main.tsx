03a67bc7b566be79ad03f4734b5554e440999416
mini-777 <pho0570@gmail.com>
2023-06-22 15:03:17 +0900

    Update expo

--- frontend/DEPS/src/screen/Main.tsx
+++ frontend/DEPS/src/screen/Main.tsx
@@ -1,15 +1,42 @@
-import React, { useState, useEffect, createRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useRef,
+  useCallback,
+} from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { Platform, StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View, Text } from 'react-native';
 import * as Location from 'expo-location';
+import BottomSheet from '@gorhom/bottom-sheet';
 import Device from 'expo-device';
-import { Box, Icon, Input, ZStack } from 'native-base';
+import {
+  Box,
+  Center,
+  HStack,
+  Icon,
+  Input,
+  Skeleton,
+  VStack,
+} from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios';
+
+const baseURL = 'https://hg3498-app.azurewebsites.net/api/';
 
 export default function Main() {
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState({
+    coords: { latitude: 0, longitude: 0 },
+  });
+  const [chargeLocation, setChargeLocation] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
   useEffect(() => {
     (async () => {
       if (Platform.OS === 'android' && !Device.isDevice) {
@@ -26,12 +53,20 @@ export default function Main() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+      getChargeLocation();
     })();
   }, []);
 
+  const getChargeLocation = async () => {
+    await axios.get(`${baseURL}carCenter`).then((res) => {
+      setChargeLocation(res.data);
+    });
+  };
+
   return (
     <Box style={styles.container}>
-      <Box style={styles.overlay}>
+      <Box style={styles.topOverlay}>
         <Input
           size='2xl'
           placeholder='충전소 검색'
@@ -51,12 +86,84 @@ export default function Main() {
       </Box>
       <Box>
         <MapView
-          followsUserLocation={true}
           showsUserLocation={true}
+          followsUserLocation={true}
+          onMarkerPress={(e) => {
+            console.log(e.nativeEvent);
+            setModalVisible(true);
+          }}
+          camera={{
+            center: {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            },
+            pitch: 40,
+            heading: 20,
+            // Only on iOS MapKit, in meters. The property is ignored by Google Maps.
+            altitude: 3000,
+
+            // Only when using Google Maps.
+            zoom: 15,
+          }}
           style={styles.map}
         >
-          <Marker coordinate={location}></Marker>
+          {chargeLocation.map((item, index) => (
+            <Marker
+              title='YIKES, Inc.'
+              description='Web Design and Development'
+              image={require('../image/marker.png')}
+              key={index}
+              coordinate={{
+                latitude: item.latitude,
+                longitude: item.longitude,
+              }}
+            />
+          ))}
         </MapView>
+      </Box>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
+      <Box style={styles.bottomOverlay}>
+        <Center w='100%'>
+          {/* <HStack
+            w='100%'
+            maxW='400'
+            borderWidth='1'
+            space={8}
+            rounded='md'
+            _dark={{
+              borderColor: 'coolGray.500',
+            }}
+            _light={{
+              borderColor: 'coolGray.200',
+            }}
+            p='4'
+          >
+            <VStack flex='3' space='4'>
+              <Skeleton startColor='amber.300' />
+              <Skeleton.Text />
+              <HStack space='2' alignItems='center'>
+                <Skeleton size='5' rounded='full' />
+                <Skeleton h='3' flex='2' rounded='full' />
+                <Skeleton
+                  h='3'
+                  flex='1'
+                  rounded='full'
+                  startColor='indigo.300'
+                />
+              </HStack>
+            </VStack>
+            <Skeleton flex='1' h='150' rounded='md' startColor='coolGray.100' />
+          </HStack> */}
+        </Center>
       </Box>
     </Box>
   );
@@ -70,7 +177,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  overlay: {
+  topOverlay: {
     position: 'absolute',
     top: 0,
     left: 0,
@@ -82,5 +189,21 @@ const styles = StyleSheet.create({
     marginTop: 60,
     marginHorizontal: 20,
     borderRadius: 30,
+  },
+  bottomOverlay: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    backgroundColor: 'white',
+    zIndex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 30,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
 });
